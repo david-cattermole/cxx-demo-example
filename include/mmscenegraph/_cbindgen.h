6afc65e01d803aa185a39ee9a221239ac64b980d
david-cattermole <cattermole91@gmail.com>
2020-12-29 19:40:04 +0000

    Change cbindgen to use C++ macro for cross platform public symbols.

--- include/mmscenegraph/_cbindgen.h
+++ include/mmscenegraph/_cbindgen.h
@@ -3,7 +3,9 @@
 #include <cstdarg>
 #include <cstdint>
 #include <cstdlib>
+#include <ostream>
 #include <new>
+#include "symbol_export.h"
 
 namespace mmscenegraph {
 
@@ -14,9 +16,9 @@ struct SceneGraph {
 
 extern "C" {
 
-void scene_graph_delete(SceneGraph *scene_graph_ptr);
+MMSCENEGRAPH_SYMBOL_EXPORT SceneGraph *scene_graph_new(int id);
 
-SceneGraph *scene_graph_new(int id);
+MMSCENEGRAPH_SYMBOL_EXPORT void scene_graph_delete(SceneGraph *scene_graph_ptr);
 
 } // extern "C"
 
