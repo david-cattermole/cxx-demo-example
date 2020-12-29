c94761bf3b35964f2eddc76bf5119b9fad273dc0
david-cattermole <cattermole91@gmail.com>
2020-12-29 19:42:55 +0000

    Use C++ symbol export macro in C++ code.

--- include/mmscenegraph/_cpp.h
+++ include/mmscenegraph/_cpp.h
@@ -4,14 +4,16 @@
 #include <cxx.h>
 #include <memory>
 #include <string>
-
+#include "symbol_export.h"
 
 namespace mmscenegraph {
 
     class ThingC {
     public:
+        MMSCENEGRAPH_SYMBOL_EXPORT
         ThingC(std::string appname);
 
+        MMSCENEGRAPH_SYMBOL_EXPORT
         ~ThingC();
 
         std::string appname;
@@ -19,10 +21,13 @@ namespace mmscenegraph {
 
     struct SharedThing;
 
+    MMSCENEGRAPH_SYMBOL_EXPORT
     std::unique_ptr <ThingC> make_demo(rust::Str appname);
 
+    MMSCENEGRAPH_SYMBOL_EXPORT
     const std::string &get_name(const ThingC &thing);
 
+    MMSCENEGRAPH_SYMBOL_EXPORT
     void do_thing(SharedThing state);
 
 } // namespace mmscenegraph
